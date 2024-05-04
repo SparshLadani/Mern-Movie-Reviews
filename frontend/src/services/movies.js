d541b06404c52c6a8264c4e5bf4bff3f39e8bac9
SparshLadani <141569599+SparshLadani@users.noreply.github.com>
2024-05-04 16:21:35 -0300

    Update movies.js

--- frontend/src/services/movies.js
+++ frontend/src/services/movies.js
@@ -2,31 +2,31 @@ import axios from "axios";
 
 class MovieDataService{
     getAll(page = 0){
-        return axios.get(`http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies?page=${page}`)
+        return axios.get(`http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies?page=${page}`)
     }
 
     get(id){
-        return axios.get(`http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies/id/${id}`)
+        return axios.get(`http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies/id/${id}`)
     }
 
     find(query, by = "title", page = 0){
-        return axios.get(`http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies?${by}=${query}&page=${page}`)
+        return axios.get(`http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies?${by}=${query}&page=${page}`)
     }
 
     createReview(data){
-    return axios.post("http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies/review", data)
+    return axios.post("http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies/review", data)
     }
 
     updateReview(data){
-    return axios.put("http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies/review", data)
+    return axios.put("http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies/review", data)
     }
 
     deleteReview(id, userId){
-    return axios.delete("http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies/review",{data:{review_id: id, user_id: userId}})
+    return axios.delete("http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies/review",{data:{review_id: id, user_id: userId}})
     }
 
     getRatings(){
-    return axios.get("http://https://nameless-eyrie-04394-1f953de8842f.herokuapp.com/api/v1/movies/ratings")
+    return axios.get("http://https://mern-movie-reviews-c8bd646efd24.herokuapp.com/api/v1/movies/ratings")
     }
 }
 
